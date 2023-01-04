5c726bfb346c336d00594da69f0f325197fb76be
Aloun <alounv@gmail.com>
2023-01-04 23:59:27 +0100

    day 24 - 1

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ pub mod d_20;
 pub mod d_21;
 pub mod d_22;
 pub mod d_23;
+pub mod d_24;
 pub mod d_3;
 pub mod d_4;
 pub mod d_5;
@@ -44,6 +45,7 @@ fn main() {
         "21" => d_21::main(),
         "22" => d_22::main(),
         "23" => d_23::main(),
+        "24" => d_24::main(),
         _ => println!("I don't know what to say!"),
     }
 }
